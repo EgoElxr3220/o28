88557d8d7e70b00a47a1d23617bd20b1506e3ca2
Lance Boylan <eg01st3220@gmail.com>
2026-03-31 16:22:58 -0600

    finished

--- o28/Program.cs
+++ o28/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using o28;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var intBox = new StorageBox<int>();
+        intBox.Store(580);
+        var item1 = intBox.Retrieve();
+        Console.WriteLine(item1.ToString());
+
+        var stringBox = new StorageBox<string>();
+        stringBox.Store("Your mom");
+        var item2 = stringBox.Retrieve();
+        Console.WriteLine(item2.ToString());
     }
 }
---
+++ o28/StorageBox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace o28
+{
+    internal class StorageBox<T>
+    {
+        private T Item;
+
+        public void Store(T item)
+        {
+            Item = item;
+        }
+
+        public T Retrieve()
+        {
+            return Item; 
+        }
+    }
+}
